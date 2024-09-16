b5dc58f0b37ece9014570feb55a952e207aa36af
Kevin Yang <e510021@gmail.com>
2024-09-17 02:51:09 +0800

    Implement invoice CRUD

---
+++ app/lib/actions.ts
@@ -0,0 +1,72 @@
+'use server';
+
+import { z } from 'zod';
+import { sql } from '@/db';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+// creates a new schema but omits id and date since it's not provided in the form
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  // zod will throw an error if any field is missing or wrong
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql(`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES ('${customerId}', '${amountInCents}', '${status}', '${date}')
+`);
+
+  // clear next.js cache for this url and will fetch new data when redirecting there
+  revalidatePath('/dashboard/invoices');
+
+  // go to this page
+  redirect('/dashboard/invoices');
+}
+
+// expected update types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function editInvoice(invoiceId: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql(`
+    UPDATE invoices
+    SET amount = '${amountInCents}', status = '${status}', customer_id = '${customerId}'
+    WHERE id = '${invoiceId}'
+  `);
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(invoiceId: string) {
+  await sql(`
+    DELETE FROM invoices WHERE id = '${invoiceId}'  
+  `);
+
+  // you don't need to redirect if it is the current path
+  // this will trigger a new request and re-render the table
+  revalidatePath('/dashboard/invoices');
+}
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { editInvoice } from '@/app/lib/actions';
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,8 +18,10 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const editInvoiceAction = editInvoice.bind(null, invoice.id);
+
   return (
-    <form>
+    <form action={editInvoiceAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
